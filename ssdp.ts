164b6886fa3933975e6af5a817c749d50eae175b
Daniel N <2color@users.noreply.github.com>
2025-06-06 11:11:33 +0200

    add reproduction of udp multicast bug

--- ssdp.ts
+++ ssdp.ts
@@ -1,13 +1,41 @@
-import dgram from 'node:dgram'
+import { createSocket } from 'node:dgram'
 
-const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true })
 
-socket.bind(1900, () => {
+const ssdpSocketOptions = {
+  broadcast: {
+    address: '239.255.255.250',
+    port: 1900
+  },
+    bind: {
+      address: '0.0.0.0',
+      port: 1900
+    },
+  maxHops: 4
+}
+
+const socket = createSocket({
+  type: 'udp4',
+  reuseAddr: true,
+}, (buf, info) => {
+  console.log('transport:incoming-message', buf, info)
+})
+
+socket.bind(ssdpSocketOptions.bind.port, ssdpSocketOptions.bind.address)
+
+socket.on('error', (err) => {
+  console.log('error', err)
+})
+
+socket.on('listening', () => {
   try {
-    socket.addMembership('239.255.255.250', '0.0.0.0')
-    // socket.addMembership('239.255.255.250', '192.168.3.65')
-    console.log('Joined multicast group')
-  } catch (err) {
-    console.error('addMembership failed:', err)
+    console.log(ssdpSocketOptions.broadcast.address, socket.address().address)
+    socket.addMembership(ssdpSocketOptions.broadcast.address, socket.address().address)
+    // socket.addMembership(ssdpSocketOptions.broadcast.address)
+    socket.setBroadcast(true)
+    socket.setMulticastTTL(ssdpSocketOptions.maxHops)
+
+    console.log('listening')
+  } catch (error: unknown) {
+    console.log('error', error)
   }
 })
